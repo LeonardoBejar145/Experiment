fadbf6a7f5ce7acb9e7efc5a90f93f017329073b
LeonardoBejar145 <Leonardo.Bejar@gisma-student.com>
2025-07-03 09:36:08 -0600

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,1 +1,10 @@
+a = int(input("write a number as you please: ")) 
+b = int(input("write a number as you please: ")) 
+c = int(input("write a number as you please: ")) 
 
+if "a" > "b" and "a" > "c":
+  print ("The biggest number is a")
+if "b" > "a" and "b" > "c":
+  print("The biggest number is b")
+if "c" > "a" and "c" > "b":
+  print("The biggest number is c")
